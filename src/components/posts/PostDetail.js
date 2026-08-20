ffe621b6abae981e3640218c8e5752483fefd601
priyaaggarwal2910 <priyaaggarwal2910@gmail.com>
2026-08-19 19:46:30 -0500

    commit
    Link author name to profile

--- src/components/posts/PostDetail.js
+++ src/components/posts/PostDetail.js
@@ -41,7 +41,7 @@ export const PostDetail = ({ isAdmin }) => {
       <div className="container">
         <h1 className="title">{post.title}</h1>
         <p className="subtitle">
-          By {post.user.username}
+          By <Link to={`/profiles/${post.user.id}`}>{post.user.username}</Link>
           {post.category && <> &middot; {post.category.label}</>}
           {post.publication_date && (
             <> &middot; {formatPublicationDate(post.publication_date)}</>
